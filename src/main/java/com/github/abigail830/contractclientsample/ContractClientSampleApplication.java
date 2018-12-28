e2b3d4be4d1853ecdfeb283db16085076373ea43
SaraQian <abigail830@163.com>
2018-12-28 15:56:24 +0800

    init commit

---
+++ src/main/java/com/github/abigail830/contractclientsample/ContractClientSampleApplication.java
@@ -0,0 +1,14 @@
+package com.github.abigail830.contractclientsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractClientSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractClientSampleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/github/abigail830/contractclientsample/ContractClientSampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.abigail830.contractclientsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContractClientSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
