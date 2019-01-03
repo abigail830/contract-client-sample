bda24055c5a797ae1e9de3e2e57486b7bf2e65ea
SaraQian <abigail830@163.com>
2019-01-03 17:59:25 +0800

    point to contract-listener to get contracts

--- src/test/java/com/github/abigail830/contractclientsample/contract/consumer/ContractConsumer1Test.java
+++ src/test/java/com/github/abigail830/contractclientsample/contract/consumer/ContractConsumer1Test.java
@@ -1,6 +1,7 @@
 package com.github.abigail830.contractclientsample.contract.consumer;
 
 import com.github.abigail830.contractclientsample.ContractClientSampleApplicationTests;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,21 +16,20 @@ import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.Map;
 
-@Ignore
 @RunWith(SpringRunner.class)
 @EnableAutoConfiguration
 @SpringBootTest (classes= ContractClientSampleApplicationTests.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureStubRunner(ids=("com.github.abigail830:mock-server-sample:+:stubs:6565"),
-//        repositoryRoot="http://nexus.saraqian.cn/repository/maven-snapshots/",
-//        stubsMode = StubRunnerProperties.StubsMode.REMOTE
-        stubsMode = StubRunnerProperties.StubsMode.LOCAL
+        repositoryRoot="http://nexus.saraqian.cn/repository/maven-snapshots/",
+        stubsMode = StubRunnerProperties.StubsMode.REMOTE
+//        stubsMode = StubRunnerProperties.StubsMode.LOCAL
 )
 public class ContractConsumer1Test {
 
     RestTemplate restTemplate = new RestTemplate();
 
     @Test
-    public void testGetServiceNameFromContractProvider(){
+    public void testGetServiceNameFromContractConsumer(){
         Map<String, String> parameterizedType = new HashMap<>();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -38,6 +38,7 @@ public class ContractConsumer1Test {
         ResponseEntity<String> responseEntity = restTemplate
                 .exchange("http://127.0.0.1:6565/info/name", HttpMethod.GET, requestEntity, String.class, parameterizedType);
 
-        System.out.println(responseEntity.getBody());
+        String abc = "{\"name\":\"contract-consumer\"}";
+        Assert.assertEquals(abc, responseEntity.getBody());
     }
 }
